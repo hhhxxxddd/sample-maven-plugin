eff9cf68f8d16af21045cf20f351111eff016b31
hhhxxxddd <hxd476571715@live.com>
2022-11-28 08:09:01 +0800

    feat:插件绑定clean周期

--- src/main/java/priv/hhhxxxddd/plugin/SamplePlugin.java
+++ src/main/java/priv/hhhxxxddd/plugin/SamplePlugin.java
@@ -3,6 +3,7 @@ package priv.hhhxxxddd.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,7 @@ import org.slf4j.LoggerFactory;
  * @apiNote <p>{@link #execute()} 插件执行</p>
  * @since 2022-11-12 01:54
  */
-@Mojo(name = "say-hi")
+@Mojo(name = "say-hi", defaultPhase = LifecyclePhase.CLEAN)
 public class SamplePlugin extends AbstractMojo {
     private static final Logger logger = LoggerFactory.getLogger(SamplePlugin.class.getSimpleName());
 
